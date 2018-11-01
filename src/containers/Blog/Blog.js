ead85640038e14ae6f29336bc655719b4334ad91
777z <7b.onus583@gmail.com>
2018-11-01 19:26:44 +0100

    fetch+Post+Catch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,9 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount(){
@@ -25,22 +27,39 @@ class Blog extends Component {
                 this.setState({posts: updatedPosts})
             // console.log(response)
         })
+        .catch(error => {
+            // console.log(error)
+            this.setState({
+                error: true
+            })
+        })
         
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({
+            selectedPostId: id
+        })
     }
 
     render () {
 
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+        let posts = <p style={{textAlign: 'center'}}> Error: Something went wrong!</p>
+        if (!this.state.error){
+            posts = this.state.posts.map(post => {
+            return <Post key={post.id} 
+                         title={post.title} 
+                         author={post.author}
+                         clickd={() => this.postSelectedHandler(post.id)} />
         })
-
+    }
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
@@ -48,6 +67,7 @@ class Blog extends Component {
             </div>
         );
     }
+
 }
 
 export default Blog;
