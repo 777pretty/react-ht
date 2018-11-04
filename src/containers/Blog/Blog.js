c36e4caab8fa48274732b2b2cacba6e6cc12f1a5
777z <7b.onus583@gmail.com>
2018-11-04 14:29:00 +0100

    Implement Components Asynchronously

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,14 +4,17 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-// import FullPost from './FullPost/FullPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -36,7 +39,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>SDDDSAD</h1>} /> */}
                 <Switch>
-                   {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null} 
+                   {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null} 
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h2>404: Not found</h2>}/>
                     {/* <Redirect from="/" to="/posts" /> */}
