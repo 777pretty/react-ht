a8d2ba02d536721891abb3b3789c418d6d24f032
777z <7b.onus583@gmail.com>
2018-11-04 13:25:56 +0100

    Added Redirect for the #404 case

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -38,7 +38,8 @@ class Blog extends Component {
                 <Switch>
                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null} 
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h2>404: Not found</h2>}/>
+                    {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
             </div>
         );
